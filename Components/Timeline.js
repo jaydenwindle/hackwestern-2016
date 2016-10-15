3e40150fb4ebc4440ccb434c73e4fd6bd77e651b
Jayden Windle <jaydenwindle@gmail.com>
2016-10-15 11:21:15 -0400

    LISTVIEW IS WORKING

--- Components/Timeline.js
+++ Components/Timeline.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 
-import { Container, Content, Text, ListView } from 'native-base';
+import { Container, Content, Text} from 'native-base';
 
 import ContactCard from './ContactCard';
 
+var data = [
+    {
+        img: require(`../img/face1.jpg`),
+        name:'John James',
+        desc: "cool guy",
+    },
+    {
+        img: require(`../img/face2.jpg`),
+        name:'Barry Allen',
+        desc: "even cooler guy",
+    },
+    {
+        img: require(`../img/face3.jpg`),
+        name:'Clark Kent',
+        desc: "the coolest guy",
+    }
+]
+
 export default class Timeline extends Component {
 
-  _generateCards() {
-    var cards = [];
-    var img1 = require(`../img/face1.jpg`);
-    var img2 = require(`../img/face2.jpg`);
-    var img3 = require(`../img/face3.jpg`);
-    var names = ['John James', 'Barry Allen', 'Clark Kent'];
-    var desc = ['Cool guy', 'Even cooler guy', 'The coolest guy'];
-    cards.push(<ContactCard source={img1} name={names[0]} description={desc[0]} />);
-    cards.push(<ContactCard source={img2} name={names[1]} description={desc[1]} />);
-    cards.push(<ContactCard source={img3} name={names[2]} description={desc[2]} />);
-    return cards;
+
+ constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(data)
+    };
+    console.log(this.state.dataSource);
   }
 
   render() {
     return (
       <Container>
         <Content padder>
-          {this._generateCards()}
+          <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <ContactCard source={rowData.img} name={rowData.name} description={rowData.desc}></ContactCard>}
+          />
         </Content>
       </Container>
     );
