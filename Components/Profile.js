1de7b500f5966825ace2efa07a655da3611e8174
th9061 <fuzihang1995@gmail.com>
2016-10-15 07:04:18 -0400

    commiting with my real id

--- Components/Profile.js
+++ Components/Profile.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 
-import { Container, Content, List, ListItem, Icon, Text, Button } from 'native-base';
+import { Container, Content, List, ListItem, Icon, Text, Button, Grid } from 'native-base';
 
 
 export default class Profile extends Component { // eslint-disable-line
@@ -18,26 +18,28 @@ export default class Profile extends Component { // eslint-disable-line
     return (
       <Container>
         <Content padder>
-          <List>
-            <ListItem>
-              <Icon name="ios-person"/>
-              <Text>  Name</Text>
-            </ListItem>
-            <ListItem>
-              <Text>  Phone</Text>
-            </ListItem>
-            <ListItem>
-              <Icon name="ios-mail"/>
-              <Text>  Email</Text>
-            </ListItem>
-            <ListItem>
-              <Text>  Facebook</Text>
-            </ListItem>
-            <ListItem>
-              <Text>  Twitter</Text>
-            </ListItem>
-          </List>
-          <Button onPress={this._EditProfile}>Edit Profile</Button>
+          <Grid>
+            <List>
+              <ListItem>
+                <Icon name="ios-person"/>
+                <Text>  Name</Text>
+              </ListItem>
+              <ListItem>
+                <Text>  Phone</Text>
+              </ListItem>
+              <ListItem>
+                <Icon name="ios-mail"/>
+                <Text>  Email</Text>
+              </ListItem>
+              <ListItem>
+                <Text>  Facebook</Text>
+              </ListItem>
+              <ListItem>
+                <Text>  Twitter</Text>
+              </ListItem>
+            </List>
+            <Button onPress={this._EditProfile}>Edit Profile</Button>
+          </Grid>
         </Content>
       </Container>
     );
